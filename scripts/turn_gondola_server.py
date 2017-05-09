f44665dc83af176dd8a7ee5a5fae3e96e6610576
Wojtas <wkoziej@gmail.com>
2017-05-09 23:51:17 +0200

    wind_power_plant controller prototype - ready to change turbine direction according to wind direction

--- scripts/turn_gondola_server.py
+++ scripts/turn_gondola_server.py
@@ -3,17 +3,20 @@
 from wind_power_plant.srv import *
 from gazebo_msgs.srv import * 
 import rospy
+import time
 
 def handle_turn_gondola(req):
     print "Turning [%s]" % (req.yawInRadians)
     service = '/gazebo/apply_joint_effort' 
     rospy.wait_for_service(service)
     try:
+        sleep_seconds = 1
         start = rospy.Time(0)
-        duration = rospy.Time(1)
+        duration = rospy.Time(sleep_seconds)
         effort = req.yawInRadians;
         apply_joint_effort = rospy.ServiceProxy(service, ApplyJointEffort)
         resp = apply_joint_effort('gondola_hinge', effort, start, duration)
+        time.sleep(sleep_seconds)
         print " Resp status %s and message %s " % (resp.success, resp.status_message)
         return TurnGondolaResponse()
     
---
+++ scripts/wind_power_plant_controller.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import rospy
+
+from wind_power_plant.srv import *
+from std_msgs.msg import Float32
+import numpy as np
+from math import radians
+from math import degrees
+
+ERROR_LEVEL = 0.001
+
+
+def directions_equals(dir1, dir2, error_level):
+    return abs(dir1 - dir2) <= error_level
+
+def wind_direction_changed(wind_direction_data):
+    print " Wind direction    %s " % wind_direction_data.data
+    gondola_direction_service_name = '/wind_power_plant/gondola_direction'
+    turn_gondola_service_name = '/wind_power_plant/turn_gondola'   
+    rospy.wait_for_service(gondola_direction_service_name)
+    try:
+        gondola_direction_srv = rospy.ServiceProxy(gondola_direction_service_name, GondolaDirection)
+        gondola_direction = (gondola_direction_srv()).yawInRadians
+        wind_direction = wind_direction_data.data
+
+
+        if not directions_equals (gondola_direction, wind_direction, ERROR_LEVEL):
+
+            wind_deg = ( degrees(wind_direction) + 360) %  360;
+            gondola_deg = (degrees(gondola_direction) + 360) % 360;
+            gondola_oposite_deg = (degrees(gondola_direction) + 180) % 360;
+            
+            turn_str = "right"
+            change_dir_sign = -1
+
+            if (wind_deg - gondola_oposite_deg + 360) % 360 < 180:
+                turn_str = "left"
+                change_dir_sign = 1
+
+            rospy.wait_for_service(turn_gondola_service_name)
+            # This take a while
+            turn_gondola_srv = rospy.ServiceProxy(turn_gondola_service_name, TurnGondola)
+            print " Gondola RAD %s, DEG = %s " % (gondola_direction, gondola_deg)
+            print " Wind    RAD %s, DEG = %s " % (wind_direction, wind_deg)
+            print " %s - L (GOP %s) - R %s " % (gondola_oposite_deg - wind_deg, gondola_oposite_deg, wind_deg - gondola_oposite_deg)
+            print " Turn gondola %s, %s" % (turn_str, (wind_deg - gondola_oposite_deg + 360) % 360)
+            
+            turn_gondola_srv(change_dir_sign * 1.1)
+            
+    except:
+        print "Service call failed %s" % (sys.exc_info()[0])
+        raise
+
+        
+def wind_power_plant_controller():
+    rospy.init_node('wind_power_plant_controller', anonymous=True)
+    rospy.Subscriber("/wind/direction", Float32, wind_direction_changed, queue_size=1)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    wind_power_plant_controller()
